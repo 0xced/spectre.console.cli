e85f905d7e4bcc486bae65f417d6e48997d860bb
Cédric Luthi <cedric.luthi@gmail.com>
2026-05-08 17:35:06 +0200

    Add (failing) test for missing required option in default command
    This is basically the same test as Should_Throw_If_Required_Option_Is_Missing, but for the default command.
    
    Shouldly.ShouldAssertException
    result
        should be of type
    Spectre.Console.Cli.CommandRuntimeException
        but was
    null

--- src/Spectre.Console.Cli.Tests/CommandAppTests.Options.cs
+++ src/Spectre.Console.Cli.Tests/CommandAppTests.Options.cs
@@ -23,5 +23,25 @@ public sealed partial class CommandAppTests
                 .And(ex =>
                     ex.Message.ShouldBe("Command 'test' is missing required option 'foo'."));
         }
+
+        [Fact]
+        public void Should_Throw_If_Required_Option_Is_Missing_For_Default_Command()
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.SetDefaultCommand<GenericCommand<RequiredOptionsSettings>>();
+            fixture.Configure(config =>
+            {
+                config.PropagateExceptions();
+            });
+
+            // When
+            var result = Record.Exception(() => fixture.Run());
+
+            // Then
+            result.ShouldBeOfType<CommandRuntimeException>()
+                .And(ex =>
+                    ex.Message.ShouldBe("Missing required option 'foo'."));
+        }
     }
 }
